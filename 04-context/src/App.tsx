eb716a5384aa3a20d7b1ea99afdb4c99c35b0408
sheppardjm <jsheppard@neucadia.com>
2022-06-19 19:26:39 -0400

    Context types lesson

--- 04-context/src/App.tsx
+++ 04-context/src/App.tsx
@@ -1,25 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useContext } from 'react';
 import './App.css';
+import { InputValueContext } from './context/InputValueContext';
 
 function App() {
+  const { state, dispatch } = useContext(InputValueContext);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <p>Value: {state.inputValue}</p>
+      <button onClick={() => dispatch({ type: "SET_INPUT_VALUE_TO_100" })}>SET_INPUT_VALUE_TO_100</button>
+    </>
   );
 }
 
